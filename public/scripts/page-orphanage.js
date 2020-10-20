5195613df367a5bdaeaae182a3c75b399a7e6d0b
igor martins <igorm409@gmail.com>
2020-10-20 14:11:55 -0300

    finalização da page-orphanage css e uso e customização da api leafletjs para adicionar o mapa atraves de javascript

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,30 @@
+//create map
+const map = L.map('mapid').setView([-19.947159,-43.9619981], 15)
+
+
+// create and add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+//create icon 
+const icon = L.icon({
+    iconUrl:"./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+
+//create popup overlay
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1"class="chosse-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+
+//create and add marker
+L.marker([-19.947159,-43.9619981], {icon}).addTo(map)
+    .bindPopup(popup)
+    
